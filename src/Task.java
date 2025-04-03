17def47f03545e4fac016df67a627a9b6d8190bd
Sakura <sakurathetrans@gmail.com>
2025-04-03 13:54:09 -0400

    Initial commit

---
+++ src/Task.java
@@ -0,0 +1,37 @@
+public class Task {
+    private String title;
+    private String desc;
+    private int priority;
+
+    public Task(String title, String desc, int priority) {
+        this.title = title;
+        this.desc = desc;
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+
+}
